64036f36b790429cd29b9e51f8bb0b37abdc5c94
David Wikmans <dave@douchedata.com>
2015-12-13 20:57:54 +0100

    Function for generating terrain

--- game.js
+++ game.js
@@ -1,50 +1,4 @@
-function Perlin1D() {
-	var MAX_VERTICES = 256;
-	var MAX_VERTICES_MASK = MAX_VERTICES -1;
-	var amplitude = 1;
-	var scale = 1;
-
-	var r = [];
-
-	for ( var i = 0; i < MAX_VERTICES; ++i ) {
-		r.push(Math.random());
-	}
-
-	var getVal = function( x ){
-		var scaledX = x * scale;
-		var xFloor = Math.floor(scaledX);
-		var t = scaledX - xFloor;
-		var tRemapSmoothstep = t * t * ( 3 - 2 * t );
-
-		/// Modulo using &
-		var xMin = xFloor & MAX_VERTICES_MASK;
-		var xMax = ( xMin + 1 ) & MAX_VERTICES_MASK;
-
-		var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
-
-		return y * amplitude;
-	};
-
-	var lerp = function(a, b, t ) {
-		return a * ( 1 - t ) + b * t;
-	};
-
-	return {
-		getVal: getVal,
-		setAmplitude: function(newAmplitude) {
-			amplitude = newAmplitude;
-		},
-		setScale: function(newScale) {
-			scale = newScale;
-		}
-	};
-};
-
-
-
 Game = {};
-
-var terrain;
 
 Game.Intro = function(){};
 Game.Menu = function(){};
@@ -124,6 +78,40 @@ Game.Play.prototype =
     //this.load.audio('pcp', '../assets/pcp.mp3');
   },
 
+  genTerrain: function(distance)
+  {
+	this.terrainSegment.push(game.add.sprite(distance, 330, 'empty'));
+	this.game.physics.p2.enableBody(this.terrainSegment[this.terrainSegment.length - 1],true);
+	this.terrainSegment[this.terrainSegment.length - 1].body.clearShapes();
+	this.terrainSegment[this.terrainSegment.length - 1].body.static = true;
+
+	var terrain = [[distance, 100]];
+	this.noise.setAmplitude(20);
+	this.noise.setScale(1);
+	for (var i = distance/50.0; i <= (distance/50.0) + 10.0; i = i+1.0)
+	{
+		var height = this.noise.getVal(i);
+		var x = i * 50.0;
+		terrain.push([x, -height]);
+	}
+	terrain.push([distance + 500, 100]);
+	console.log("WAT", terrain);
+	var graphics = game.add.graphics(distance, 330);
+
+	graphics.beginFill(0x000000);
+	graphics.lineStyle(0, 0xffd900, 1);
+
+	terrain.forEach(function(vertex) {
+		graphics.lineTo(vertex[0], vertex[1]);
+	});
+	graphics.lineTo(distance, 200);
+
+	graphics.endFill();
+
+	window.graphics = graphics;
+	this.terrainSegment[this.terrainSegment.length - 1].body.addPolygon( {}, terrain);
+  },
+
   create: function()
   {
     this.loading_text.destroy(true);
@@ -131,91 +119,86 @@ Game.Play.prototype =
     this.game.stage.backgroundColor = '#ffffff';
     this.game.physics.startSystem(Phaser.Physics.P2JS);
 	this.game.physics.p2.gravity.y = 500;
+	this.terrainSegment = [];
 
-    //this.splash = this.add.audio('splash2');
+	//this.splash = this.add.audio('splash2');
+	function Perlin1D() {
+		var MAX_VERTICES = 256;
+		var MAX_VERTICES_MASK = MAX_VERTICES -1;
+		var amplitude = 1;
+		var scale = 1;
 
-    //create layer
-    //this.backgroundLayer = this.map.createLayer('backgroundLayer');
-    //console.log(this.backgroundLayer);
-    //this.backgroundLayer.worldScale(0.25);
-    //this.blockedLayer = this.map.createLayer('blockedLayer');
-    this.world.setBounds(0,0,10000, 400);
+		var r = [];
 
-    //this.map.setCollisionBetween(1, 100000, true, this.blockedLayer);
+		for ( var i = 0; i < MAX_VERTICES; ++i ) {
+			r.push(Math.random());
+		}
 
-    //create player
-    //var playerStart = objects('playerStart', this.map, 'objectsLayer')
+		var getVal = function( x ){
+			var scaledX = x * scale;
+			var xFloor = Math.floor(scaledX);
+			var t = scaledX - xFloor;
+			var tRemapSmoothstep = t * t * ( 3 - 2 * t );
 
-    this.player = this.game.add.sprite(10,10, 'grisrunner');
+			/// Modulo using &
+			var xMin = xFloor & MAX_VERTICES_MASK;
+			var xMax = ( xMin + 1 ) & MAX_VERTICES_MASK;
 
-    this.player.anchor.setTo(0.5);
-    //this.player.animations.add('left', ['iceman-left-1.png', 'iceman-left-2.png'], 5, true);
-    //this.player.animations.add('right', ['iceman-right-1.png', 'iceman-right-2.png'], 5, true);
-    //this.player.animations.add('back', ['iceman-back-1.png', 'iceman-back-2.png'], 5, true);
-    //this.player.animations.add('front', ['iceman-front-1.png', 'iceman-front-2.png'], 5, true);
-    //this.player.animations.play('front');
+			var y = lerp( r[ xMin ], r[ xMax ], tRemapSmoothstep );
+
+			return y * amplitude;
+		};
+
+		var lerp = function(a, b, t ) {
+			return a * ( 1 - t ) + b * t;
+		};
+
+		return {
+			getVal: getVal,
+			setAmplitude: function(newAmplitude) {
+				amplitude = newAmplitude;
+			},
+			setScale: function(newScale) {
+				scale = newScale;
+			}
+		};
+	};
+
+	this.noise = Perlin1D();
+
+	this.world.setBounds(0,0,10000, 400);
+
+	this.player = this.game.add.sprite(10,10, 'grisrunner');
+
+	this.player.anchor.setTo(0.5);
+	//this.player.animations.add('left', ['iceman-left-1.png', 'iceman-left-2.png'], 5, true);
+	//this.player.animations.add('right', ['iceman-right-1.png', 'iceman-right-2.png'], 5, true);
+	//this.player.animations.add('back', ['iceman-back-1.png', 'iceman-back-2.png'], 5, true);
+	//this.player.animations.add('front', ['iceman-front-1.png', 'iceman-front-2.png'], 5, true);
+	//this.player.animations.play('front');
 	this.game.physics.p2.enableBody(this.player,false);
 
-	//this.line = new Phaser.Line(0, 100, 100, 200);
-	this.line = game.add.sprite(0, 330, 'empty');
-	this.game.physics.p2.enableBody(this.line,false);
-	this.line.body.clearShapes();
-	//this.line.body.mass = 0;
-	this.line.body.static = true;
+	this.game.camera.follow(this.player);
 
-	terrain = [[0, 100]];
-	var noise = Perlin1D();
-	noise.setAmplitude(20);
-	noise.setScale(1);
-	for (var i = 0.0; i <= 10.0; i = i+1.0)
-	{
-		var height = noise.getVal(i);
-		var x = i * 50.0;
-		terrain.push([x, -height]);
-		//console.log("Height", height, "i", i, terrain);
-	}
-	terrain.push([500, 100]);
-	console.log("WAT", terrain);
-	var t2 = terrain.concat();
-	//this.line.body.addLine( 100, 0, 0, 0);
-	//this.line.body.addRectangle( 100, 10, 0, 0, 0);
-
-    //this.player.body.collideWorldBounds = true;
-
-    //the camera will follow the player in the world
-    this.game.camera.follow(this.player);
-
-    //move player with cursor keys
-    this.cursors = this.game.input.keyboard.createCursorKeys();
+	//move player with cursor keys
+	this.cursors = this.game.input.keyboard.createCursorKeys();
 
 	/*
-    pcp = game.add.audio('pcp');
+	   pcp = game.add.audio('pcp');
 
-    function start() {
-      if (!playing)
-        pcp.loopFull(0.6);
-      playing = true;
-    }
+	   function start() {
+	   if (!playing)
+	   pcp.loopFull(0.6);
+	   playing = true;
+	   }
 
-    game.sound.setDecodedCallback(pcp, start, this);
-	*/
-	var graphics = game.add.graphics(0, 330);
-
-    // set a fill and line style
-    graphics.beginFill(0x000000);
-    graphics.lineStyle(0, 0xffd900, 1);
-
-	terrain.forEach(function(vertex) {
-		graphics.lineTo(vertex[0], vertex[1]);
-	});
-	graphics.lineTo(0, 200);
-
-	// draw a shape
-	graphics.endFill();
-
-
-	window.graphics = graphics;
-	this.line.body.addPolygon( {}, t2);
+	   game.sound.setDecodedCallback(pcp, start, this);
+	   */
+	  this.genTerrain(0);
+	  this.genTerrain(500);
+	  this.genTerrain(1000);
+	  this.genTerrain(1500);
+	  this.genTerrain(2000);
   },
 
   shutdown: function()
@@ -241,6 +224,7 @@ Game.Play.prototype =
   {
 	  if (this.player.body)
 	  {
+		  console.log(this.player.position);
 		  //player movement
 		  //this.player.body.velocity.y = 0;
 		  //this.player.body.velocity.x = 0;
@@ -285,163 +269,7 @@ Game.Play.prototype =
   }
 }
 
-
 var game = new Phaser.Game(640, 400, Phaser.AUTO, '', Game.Intro);
 
 game.state.add("Play", Game.Play);
 game.state.add("Intro", Game.Intro);
-
-var GrisRunnerStates = {
-	DEFAULT: "default",
-	JUMPING: "jumping",
-};
-
-var GrisRunner = {
-	pos: null,
-	vel: null,
-	acc: null,
-	sprite: null,
-	state: GrisRunnerStates.DEFAULT,
-
-	create: function(x, y) {
-		var obj = Object.create(this);
-		obj.pos = new Victor(x, y);
-		obj.vel = new Victor(10, 0); // 10 pixels per frame to the right
-		obj.acc = new Victor(0, 0);
-		obj.sprite = new Image();
-		obj.sprite.src = "assets/grisrunner.png";
-
-		return obj;
-	},
-
-	update: function(terrainBuffer) {
-		// Add current acceleration to velocity
-		this.vel = this.vel.add(this.acc);
-		// Add current vertical velocity to vertical position
-		this.pos = this.pos.add(this.vel);
-		this.hasFootHold(terrainBuffer);
-	},
-
-	accelerate: function(accel_vector) {
-		this.acc = this.acc.add(accel_vector);
-	},
-
-	jump: function() {
-		if (this.state == GrisRunnerStates.DEFAULT) {
-			this.state = GrisRunnerStates.JUMPING;
-			this.vel.y -= 15;  // Negative is up
-		}
-	},
-
-	hasFootHold: function(terrainBuffer) {
-		var offset = this.pos.y + this.sprite.height;
-		for (var i = this.pos.x; i < this.pos.x + this.sprite.width; i++) {
-			if (offset > terrainBuffer[i]) {
-				this.acc.y = 0;
-				this.pos.y = terrainBuffer[i]-this.sprite.height;
-			} else {
-				this.acc.y = .5;
-			}
-		}
-	},
-
-	draw: function(context, viewport) {
-		context.drawImage(this.sprite, this.pos.x - viewport.pos.x, this.pos.y - viewport.pos.y);
-	},
-};
-
-function randomInRange(min, max) {
-	return Math.floor(Math.random() * max) - 1;
-};
-
-$(document).ready(function() {
-	/*
-	   var canvas = $("#canvas").get(0),
-	   context = canvas.getContext("2d"),
-	   width = canvas.width = $(window).width(),
-	   height = canvas.height = $(window).height(),
-	   viewport = {pos: new Victor(0,0), wl: new Victor(width, height)},
-	   colors = ["#ffffff", "#000000"],
-	   terrainBuffer = [],
-	   grisrunner = GrisRunner.create(0, 0),
-	   noise = Perlin1D(),
-	   highestPointBelowGrisRunner = 0,
-	   previousHighestPointBelowGrisRunner = 0;
-
-	   init();
-	   run();
-
-	   $("#canvas").click(function() {
-	   grisrunner.jump();
-	   });
-
-	   function init() {
-	   noise.setAmplitude(10);
-	   noise.setScale(2);
-	   context.fillStyle = colors[1];
-	   };
-
-	   function run() {
-		// Game loop
-		update();
-		render();
-		requestAnimationFrame(run);
-		};
-
-		function stairsTerrain(factor) {
-		for (var i = 0; i < 10; i++)
-		for (var j = 0; j < 2200; j++)
-		terrainBuffer.push(i*factor*200);
-		}
-
-		function generateTerrain() {
-		// Generates procedural terrain with perlin noise
-		var distance = 1000.0, step = 0.01;
-		for (var d = 0.0; d < distance; d = d + step) {
-		terrainBuffer.push(noise.getVal(d + 5) + height/2+100);
-		}
-		};
-
-		function needToGenerateMoreTerrain() {
-	// Here, we determine if we need to add more terrain to the terrain
-	// buffer
-	return terrainBuffer.length < 3*width;
-	}
-
-	function update() {
-	if (needToGenerateMoreTerrain()) {
-				//stairsTerrain(-1);
-				generateTerrain();
-				}
-
-				updateTerrain();
-				grisrunner.update(terrainBuffer);
-				handleCollisions();
-				};
-
-				function updateTerrain() {
-//for (var i = 0; i < grisrunner.vel.x; i++) {
-//terrainBuffer.shift();
-};
-
-function handleCollisions() {
-};
-
-function render() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	//viewport.pos.x = grisrunner.pos.x - viewport.wl.x / 2;
-	//viewport.pos.y = grisrunner.pos.y - viewport.wl.y / 2;
-	drawTerrain();
-	grisrunner.draw(context, viewport);
-};
-
-function drawTerrain() {
-	for (var i = 0; i < width; i++) {
-		context.beginPath();
-		context.moveTo(i, terrainBuffer[i]);
-		context.lineTo(i, height);
-		context.stroke();
-	}
-};
-*/
-});
